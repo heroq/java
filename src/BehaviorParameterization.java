e9980950308ff1809d815d7d6d1decb1a7ed3b1b
heroq <96118954+heroq@users.noreply.github.com>
2022-09-11 20:57:24 +0900

    동작 파라미터화_분리

---
+++ src/BehaviorParameterization.java
@@ -0,0 +1,12 @@
+// 동작 파라미터화
+
+/*
+    동작 파라미터화는 전략 디자인 패턴
+    1. pridicateApple의 interface를 생성
+    2. pridicateApple의 interface implement할 class 생성
+    3. 그리고 필터를 거칠 함수를 클래스마다 생성함
+    4. 필터를 사용할 생성한 메소드의 매개변수에 pridicateApple을 추가
+*/
+public class BehaviorParameterization {
+    
+}
--- src/stream.java
+++ src/stream.java
@@ -2,7 +2,7 @@ import java.sql.Array;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 
-public class stream {
+public class Stream {
     static class Apple{
         String color;
         public void setColor(String color){this.color = color;}
@@ -20,15 +20,7 @@ public class stream {
             }
         }
         return result;
-    }
-    /*
-        동작 파라미터화는 전략 디자인 패턴
-        1. pridicateApple의 interface를 생성
-        2. pridicateApple의 interface implement할 class 생성
-        3. 그리고 필터를 거칠 함수를 클래스마다 생성함
-        4. 필터를 사용할 생성한 메소드의 매개변수에 pridicateApple을 추가
-    */
-    
+    }    
     /*
         // stream 예제
 
