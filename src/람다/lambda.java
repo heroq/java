5cc18af0cd82685d8592037cc362d5d670689363
heroq <96118954+heroq@users.noreply.github.com>
2022-09-13 12:48:50 +0900

    람다식

--- src/람다/lambda.java
+++ src/람다/lambda.java
@@ -16,26 +16,45 @@ public class lambda {
         return setGame;
     }
 
+    public <T> List<T> filter(List<T> t, Predicate<T> p){
+        List<T> temp = new ArrayList<T>();
+        for(T get : t){
+            if(p.test(get)){
+                temp.add(get);
+            }
+        }
+        return temp;
+    }
 
     public void start(){
         람다.lambda list = new 람다.lambda();
         ArrayList<Game> games = list.lambda();
 
-        // stream의 filter의 Predicate 람다식
+        // 1.   stream의 filter의 Predicate 람다식
+        //      Predicate를 바로 생성한것
         games = (ArrayList<Game>) games.stream().filter((Game g) -> g.getLevel() <= 10).collect(Collectors.toList());
         for(Game a : games) System.out.println(a.toString());
 
-        // Predicate는 test, or, and로 되어있음
-        // boolean으로 판별해서 filter에서 Predicate로 값을 골라주네
+        // 2.   Predicate는 test, or, and로 되어있음
+        //      boolean으로 판별해서 filter에서 Predicate로 값을 골라줌
         Predicate<Game> p = (Game g) -> g.getLevel() <= 10;
         Game game = new Game("TEST", 10, "TEST2");
         boolean get = p.test(game);
         System.out.println(get);
 
+        // 3.   collect로 toList화
+        //      interface에 arraylist가 있으니깐 변환 후 받아옴
+        //      애초에 List로 받으면 캐스팅 안함
+        //      Predicate를 위에서 선언하고 가져와서 사용했음
         List<Game> games2 = list.lambda();
-        // collect로 toList화
-        // interface에 arraylist가 있으니깐 변환 후 받아옴
-        games = (ArrayList<Game>) games2.stream().filter(p).collect(Collectors.toList());
-        for(Game g : games) System.out.println(g.toString());
+        games2 = games2.stream().filter(p).collect(Collectors.toList());
+        for(Game g : games2) System.out.println(g.toString());
+
+        // 4.   filter 제네릭 메소드 추가
+        //      전략 디자인 패턴 방식
+        //      lambdaName의 메소드를 넘겨주는방식
+        List<Game> games3 = list.lambda();
+        games3 = filter(games3, new lambdaName());
+        for(Game g : games3) System.out.println(g.toString());
     }
 }
